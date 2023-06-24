e37506c9e0a512a23ae27fcef58239b858395e15
sambit2002 <sambit2002dash@gmail.com>
2023-06-24 09:57:28 +0530

    source files

--- src/API/index.js
+++ src/API/index.js
@@ -1,6 +1,6 @@
-// export const getOrders = () => {
-//   return fetch("https://dummyjson.com/carts/1").then((res) => res.json());
-// };
+export const getOrders = () => {
+  return fetch("https://dummyjson.com/carts/1").then((res) => res.json());
+};
 
 export const getRevenue = () => {
   return fetch("https://dummyjson.com/carts").then((res) => res.json());
